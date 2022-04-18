1cbd93c2c06e615aa7dd3b4f8aa9966d78f2bf63
abhisheksatpute369 <abhisheksatpute99@gmail.com>
2022-04-18 23:52:09 +0530

    update code

--- datepicker/src/App.js
+++ datepicker/src/App.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
-import DatePicker from 'react-datepicker'
-import 'react-date-picker\dist\DatePicker.css'
+import React, { useState } from "react";
+import DatePicker from "react-date-picker";
 
-import './App.css'
+import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {date: new Date()};
-      this.dateChange = this.dateChange.bind(this);}
-      dateChange(date) {
-          this.setState({date: date},
-          () => {
-                  console.log(this.state.date);
-                });
-         }
-  render() {
-      return (
-            <div>
-              <p> Pick a date:</p>
-              <DatePicker selected={this.state.date} onChange={this.dateChange} dateFormat="MMMM d, yyyy"/>
-            </div>
-     );
-    }
-  }
-export default App;
+export default function App() {
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    <div className="App">
+      <div className="datediv">
+        <DatePicker
+          id="date"
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+        />
+        <DatePicker
+          id="date"
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+        />
+      </div>
+    </div>
+  );
+}
